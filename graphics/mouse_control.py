164b51f87716685a0b6729cdef3526074963f09d
Zaid kamil <xaidmetamorphos@gmail.com>
2020-12-07 15:53:25 +0530

    update #4

--- graphics/mouse_control.py
+++ graphics/mouse_control.py
@@ -1,14 +1,20 @@
 from turtle import *
 import random
 
+speed('fastest')
+s = Screen()
+s.setup(700,700)
+
+fillcolor('red')
 def create_flower(x,y):
     penup()
     goto(x,y)
     pendown()
     radius = random.randint(5,30)
-    fillcolor('red')
     begin_fill()
     for i in range(6):
         circle(radius)
         left(60)
     end_fill()
+s.onclick(create_flower)
+mainloop()
